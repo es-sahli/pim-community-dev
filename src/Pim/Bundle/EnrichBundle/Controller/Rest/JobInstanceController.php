d286dbd5da6c5878d1df36fbf63cb305fc349a33
Anael CHARDAN <anael.chardan@gmail.com>
2017-07-17 13:36:38 -0400

    TIP-747: Fix Validator things

--- src/Pim/Bundle/EnrichBundle/Controller/Rest/JobInstanceController.php
+++ src/Pim/Bundle/EnrichBundle/Controller/Rest/JobInstanceController.php
@@ -22,6 +22,7 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\Constraints\Valid;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
@@ -417,7 +418,7 @@ class JobInstanceController
             }
         }
 
-        $globalViolations = $this->validator->validate($jobInstance, ['Default']);
+        $globalViolations = $this->validator->validate($jobInstance, new Valid(), ['Default']);
         if ($globalViolations->count() > 0) {
             foreach ($globalViolations as $error) {
                 $errors[$error->getPropertyPath()] = $error->getMessage();
